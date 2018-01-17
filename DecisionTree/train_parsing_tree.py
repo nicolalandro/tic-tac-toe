b1f3e0648585e272d0600c9a1b1360a089df2f92
nicolalandro <nicolaxx94@live.it>
2018-01-17 11:19:25 +0100

    new addestred ai

--- DecisionTree/train_parsing_tree.py
+++ DecisionTree/train_parsing_tree.py
@@ -1,12 +1,27 @@
+import ast
+
 from sklearn.externals import joblib
 from sklearn import tree
 
+
+def read_features_from_file():
+    lines = [ast.literal_eval(line.rstrip('\n')) for line in open("training_data/features.ls")]
+    return lines
+
+
+def read_labels_from_file():
+    lines = [ast.literal_eval(line.rstrip('\n')) for line in open("training_data/labels.ls")]
+    return lines
+
+
 # X=1 O=2
-features = [[0, 0, 0, 0, 1, 0, 0, 0, 0], [2, 1, 0, 0, 1, 0, 0, 0, 0]]
-label = [0, 7]
+# features = [[0, 0, 0, 0, 1, 0, 0, 0, 0], [2, 1, 0, 0, 1, 0, 0, 0, 0]]
+# label = [0, 7]
+features = read_features_from_file()
+labels = read_labels_from_file()
 
 clf = tree.DecisionTreeClassifier()
-clf.fit(features, label)
+clf.fit(features, labels)
 
 print("Data Value:", 0, 7)
 print("Predict Value", clf.predict([[0, 0, 0, 0, 1, 0, 0, 0, 0], [2, 1, 0, 0, 1, 0, 0, 0, 0]]))
